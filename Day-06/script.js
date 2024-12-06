c366e3c5ff4b3ca59c78022847723d6c5c884cb7
Ovidijus <ovidijus.e@gmail.com>
2024-12-06 07:20:02 +0200

    Day 6

---
+++ Day-06/script.js
@@ -0,0 +1,11 @@
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.toLocaleDateString('default', { month: 'long' });
+const weekday = date.toLocaleDateString('default', { weekday: 'long' });
+const day = date.getDay() + 1;
+
+document.querySelector('.month').textContent = `${month}`;
+document.querySelector('.weekday').textContent = `${weekday}`;
+document.querySelector('.day').textContent = `${day}`;
+document.querySelector('.year').textContent = `${year}`;
